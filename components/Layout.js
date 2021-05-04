5a311ddab228f57ab1030655c9dedf45db6494dd
mcmicko <mcmzrc@gmail.com>
2021-05-04 08:55:52 +0200

    Add Link for Github Repository

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Layout({ children }) {
   return (
@@ -13,15 +13,30 @@ export default function Layout({ children }) {
             <h2>Spread The Joy</h2>
           </a>
         </Link>
+        <br />
+        <Link href="https://github.com/mcmicko/nextjs-contentful">
+          <a className="gitHub">GitHub Repository For This Project</a>
+        </Link>
       </header>
 
-      <div className="page-content">
-        { children }
-      </div>
+      <div className="page-content">{children}</div>
 
       <footer>
         <p>Copyright 2021 Just Add Marmite :)</p>
       </footer>
+
+      <style jsx>{`
+        .gitHub {
+          margin-top: 20px;
+          border: 2px solid black;
+          padding: 15px;
+          transition: 500ms ease;
+        }
+        .gitHub:hover {
+          background: black;
+          color: yellow;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
